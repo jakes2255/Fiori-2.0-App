6d8d5cd1418335724583f0597852a95e9bfaba0b
jakes2255 <aj727in@gmail.com>
2019-06-28 09:13:16 +0000

    table and OData check

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+			"sap/ui/core/mvc/Controller",
+	'sap/ui/model/json/JSONModel'
+		], function (Controller,JSONModel) {
+			"use strict";
 
-	return Controller.extend("com.demo.Z_Fiori2_Inital_App.controller.App", {
-		onInit: function () {
-
-		}
-	});
-});
+			return Controller.extend("com.demo.Z_Fiori2_Inital_App.controller.App", {
+					onInit: function () {
+						var oProductsModel;
+						// set products demo model on this sample
+						oProductsModel = new JSONModel(
+								"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/products.json"); 
+						oProductsModel.setSizeLimit(1000); 
+						this.getView().setModel(oProductsModel, 'products');
+						debugger;
+						}
+					});
+			});
