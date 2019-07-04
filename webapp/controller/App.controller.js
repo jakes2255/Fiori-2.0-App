1f9a0c0227ae8fc58507a110deabcb2ae36f57c9
jakes2255 <aj727in@gmail.com>
2019-07-04 13:14:38 +0000

    updates to App.controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,20 @@
 sap.ui.define([
+	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+], function (JSONModel, Controller) {
 	"use strict";
 
 	return Controller.extend("com.demo.Z_Fiori2_Inital_App.controller.App", {
 		onInit: function () {
-
+			this.oOwnerComponent = this.getOwnerComponent();
+			this.oRouter = this.oOwnerComponent.getRouter();
+			this.oRouter.attachRouteMatched(this.onRouteMatched, this);
+		},
+		onRouteMatched: function (oEvent){
+			
+		},
+		onExit: function () {
+			this.oRouter.detachRouteMatched(this.onRouteMatched, this);
 		}
 	});
 });
