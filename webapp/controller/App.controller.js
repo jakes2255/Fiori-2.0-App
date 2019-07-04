67513d0fa56cc103dc86e08a8c7f97a9308bd8cb
jakes2255 <aj727in@gmail.com>
2019-07-04 13:07:46 +0000

    preparing for Routing Implmementation.
    Thanks,
    Arun

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,11 @@
 sap.ui.define([
-			"sap/ui/core/mvc/Controller"
-		], function (Controller) {
-			"use strict";
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
 
-			return Controller.extend("com.demo.Z_Fiori2_Inital_App.controller.App", {
-					onInit: function () {
-						
-						debugger;
-						}
-					});
-			});
+	return Controller.extend("com.demo.Z_Fiori2_Inital_App.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
