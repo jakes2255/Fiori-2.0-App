39d73ff8a11083aa88d11cf267dd74c43362154d
jakes2255 <aj727in@gmail.com>
2019-08-20 07:11:19 +0000

    Semantic Helper provided layouts than Hard Coding the Layout.

--- webapp/controller/DetailDetail.controller.js
+++ webapp/controller/DetailDetail.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/f/library"
-], function (Controller, fioriLibrary) {
+	"sap/ui/core/mvc/Controller"
+], function (JSONModel, Controller) {
 	"use strict";
 
 	return Controller.extend("com.demo.Z_Fiori2_Inital_App.controller.DetailDetail", {
@@ -12,9 +12,9 @@ sap.ui.define([
 		 * @memberOf com.demo.Z_Fiori2_Inital_App.view.DetailDetail
 		 */
 		onInit: function () {
-			var oOwnerComponent = this.getOwnerComponent();
-			this.oRouter = oOwnerComponent.getRouter();
-			this.oModel = oOwnerComponent.getModel();
+			this.oOwnerComponent = this.getOwnerComponent();
+			this.oRouter = this.oOwnerComponent.getRouter();
+			this.oModel = this.oOwnerComponent.getModel();
 			this.oRouter.getRoute("detailDetail").attachPatternMatched(this._onPatternMatch, this);
 		},
 		_onPatternMatch: function (oEvent) {
@@ -26,7 +26,26 @@ sap.ui.define([
 			});
 		},
 		handleAboutPress: function(){
-			this.oRouter.navTo("page2", {layout:fioriLibrary.LayoutType.EndColumnFullScreen});
+			//this.oRouter.navTo("page2", {layout:fioriLibrary.LayoutType.EndColumnFullScreen});
+			var oNextUIState;
+			this.oOwnerComponent.getHelper().then(function (oHelper) {
+				oNextUIState = oHelper.getNextUIState(3);
+				this.oRouter.navTo("page2", {layout: oNextUIState.layout});
+			}.bind(this));
+		},
+		handleFullScreen: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/endColumn/fullScreen");
+			this.oRouter.navTo("detailDetail", {layout: sNextLayout, product: this._product, supplier: this._supplier});
+		},
+
+		handleExitFullScreen: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/endColumn/exitFullScreen");
+			this.oRouter.navTo("detailDetail", {layout: sNextLayout, product: this._product, supplier: this._supplier});
+		},
+
+		handleClose: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/endColumn/closeColumn");
+			this.oRouter.navTo("detail", {layout: sNextLayout, product: this._product});
 		},
 		onExit: function () {
 			this.oRouter.getRoute("detailDetail").detachPatternMatched(this._onPatternMatch, this);
